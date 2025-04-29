b30b097dffe4b2565609299e6c01339505cbe030
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-04-29 05:18:50 -0700

    Fix socket address retrieval and update route definitions in main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,19 @@ async fn main() {
 
     // Build the application with two routes and apply the CORS layer
     let app = Router::new()
-        .route("/", get(utils::root_handler))
-        .route("/randomarttest", get(utils::randomart_handler))
         .layer(cors); // Add the CORS layer to the app
+        .route("/", get(utils::root_handler))
+        .route("/randomart", get(utils::randomart_handler))
+        
 
     // Define the address to run the server on
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // println!("Listening on {}", addr);
 
+    let raw_addr = std::env::var("RUSIC_SOCKET_ADDR").unwrap;
+    let raw_port = std::env::var("RUSIC_PORT").unwrap;
+    let addr = SocketAddr::from((rawaddr, raw_port));
+    println!("Listening on {}", addr);
     // Run the server
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
